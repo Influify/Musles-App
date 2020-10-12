a8d6e863717eff88f6ac1f6910634f4e654a8102
Influify <influify@gmail.com>
2020-10-12 10:37:53 -0400

    Main

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./styles.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import Header from "./sections/Header";
 import Home from "./Home";
 import Login from "./Login";
 import SignUp from "./SignUp";
@@ -12,6 +13,7 @@ const App = () => {
     <AuthProvider>
       <Router>
         <div>
+          <Header />
           <PrivateRoute exact path="/" component={Home} />
           <Route exact path="/login" component={Login} />
           <Route exact path="/signup" component={SignUp} />
